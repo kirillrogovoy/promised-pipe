d513b69b3b50f9116c84149cc28fbcdeab48883b
Kirill Rogovoy <kirill@rogovoy.me>
2017-02-04 13:03:48 +0200

    Make benchmark more reliable

--- benchmark.js
+++ benchmark.js
@@ -3,6 +3,8 @@ const prettyTime = require('pretty-hrtime')
 const pipe = require(path.join(__dirname, process.env.PROMISED_PIPE_LIB_FILE || 'index.js'))
 
 const PIPE_SIZE = 1e3
+const CREATION_LOOP_SIZE = 1e4 * 2
+const EXECUTION_LOOP_SIZE = 1e3
 
 const inc = x => x + 1
 const fns = Array(PIPE_SIZE).fill(inc)
@@ -12,7 +14,7 @@ var t1
 // creation
 t1 = process.hrtime()
 
-for (var i = 0; i < 1e4 * 2; i++) {
+for (var i = 0; i < CREATION_LOOP_SIZE; i++) {
     pipe.apply(null, fns)
 }
 
@@ -22,8 +24,8 @@ console.log('creation:', formatTime(process.hrtime(t1)))
 t1 = process.hrtime()
 
 const fn = pipe.apply(null, fns)
-for (var i = 0; i < 1e2 * 2; i++) {
-    fn(1)
-}
 
-console.log('execution:', formatTime(process.hrtime(t1)))
+Array(EXECUTION_LOOP_SIZE).fill(1).reduce(acc => acc.then(fn), Promise.resolve(1)).then(result => {
+    console.log('execution:', formatTime(process.hrtime(t1)), 'result', result)
+})
+
